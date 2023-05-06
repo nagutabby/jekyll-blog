abc55e7992a3196f1edd7b40475a812f5ee02170
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-05-06 15:59:13 +0900

    シンタックスハイライトを追加する

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,10 @@
 import { listen } from "https://cdn.jsdelivr.net/npm/quicklink@2.3.0/dist/quicklink.mjs"
+import hljs from "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/highlight.min.js";
+
 listen();
+
+let preCodes = document.querySelectorAll("pre code:not(.hljs)");
+for (var i = 0; i < preCodes.length; i++)
+  hljs.highlightBlock(preCodes[i]);
+
+
